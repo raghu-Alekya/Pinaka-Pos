46c3d41839e8120525df9ed7148fdd53ac410f73
DESKTOP-K5964BV\user <ranjeet.chirutkar@serole.com>
2025-07-14 14:24:03 +0530

    no message

--- lib/Constants/misc_features.dart
+++ lib/Constants/misc_features.dart
@@ -2,8 +2,8 @@ import 'package:flutter/foundation.dart';
 
 class Misc{
   /// use this feature for testing only, not to be used for release builds
-  static bool enableCategoryProductWithSubCategoryList = true;
-  static bool enableDBDelete = kDebugMode ? false : false; //'true' to delete the database during development/testing, in Release pass it as false
+  static bool enableCategoryProductWithSubCategoryList = false;
+  static bool enableDBDelete = kDebugMode ? true : false; //'true' to delete the database during development/testing, in Release pass it as false
 
   printFeatures(){
     if (kDebugMode) {
